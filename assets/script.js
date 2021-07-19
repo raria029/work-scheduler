89d56822ee5433fe7f24e02695a41b63fd4725d4
raria029 <rocio.arias98@gmail.com>
2021-07-18 21:06:48 -0400

    timeblocks added

---
+++ assets/script.js
@@ -0,0 +1,23 @@
+//Table 
+
+
+// Date Displayed at the top
+
+//Time Blocks for standard business hours
+
+// Time Blocks for the current day
+
+// Color coating time blocks
+
+// Past, Present, Future color selectors (might be CSS)
+
+// Click Event for time block
+
+// Enter Event Function
+
+//Save Button
+
+//Text is saved in local storage
+
+//Saved Events carry over when page is refreshed
+    // use while loop????
